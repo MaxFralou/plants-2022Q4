dfb39f401b5573991604c24d807470fd6a7d77ef
MaxFralou <fralou@protonmail.com>
2023-01-04 17:00:24 +0300

    feat: add index.js, a little fix css

---
+++ plants/index.js
@@ -0,0 +1,6 @@
+console.log(` Score: 110 / 100\n
+- [x] The layout valid. (10/10)\n
+- [x] The layout semantic. (20/20)\n
+- [x] The layout corresponds to the layout. (48/48)\n
+- [x] Requirements for css. (12/12)\n
+- [x] Interactivity realized through css. (20/20)`);
